54af00126911e6aaa5839196af2164ec6fa94c7d
Anamta14 <156362002+Anamta14@users.noreply.github.com>
2024-12-02 01:17:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class HomePage(QMainWindow):
         loadUi("homepage.ui", self)
         self.adminbtn.clicked.connect(self.go_to_admin_signup)
         self.driverbtn.clicked.connect(self.go_to_driver_functions) 
-        self.passgrbtn.clicked.connect(self.go_to_passenger_functions) 
+        self.passgrbtn.clicked.connect(self.go_to_passenger_functions)
+        self.passgrbtn.clicked.connect(self.go_to_passenger_signup)
 
     def go_to_admin_signup(self):
         from Admin import AdminSignup 
@@ -22,8 +23,26 @@ class HomePage(QMainWindow):
         self.driver_window.show()
         self.close()
 
+    def go_to_passenger_signup(self):
+        from passengersignup import PassengerSignup  # Assuming you have a PassengerSignup class in passengersignup.py
+        self.signup_window = PassengerSignup()
+        self.signup_window.show()
+        self.close()
+
     def go_to_passenger_functions(self):
-        pass  
+        from passengeroperations import RouteBookingPage
+        self.route_management_page = RouteBookingPage(Passenger_id = 9001)
+        self.route_management_page.show()
+        self.close()  
+        self.route_management_page.tableWidget.cellDoubleClicked.connect(self.open_bus_schedule)
+        
+    def open_bus_schedule(self, row, column):
+        # Get the selected route ID from the table
+        route_id = self.route_management_page.table.item(row, 0).text()
+        from busschedule import BusSchedulePage 
+        self.bus_schedule_page = BusSchedulePage(route_id)
+        self.bus_schedule_page.show()
+        self.route_management_page.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
